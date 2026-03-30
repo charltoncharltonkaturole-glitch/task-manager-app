9ee658c3c216eddf254d7152a02b3ae5688ed20e
charlton <charlton.charlton.katurole@gmail.com>
2026-03-31 02:52:06 +0300

    Replace bcrypt with bcryptjs for Render compatibility

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const app = express();
--- setup-admin.js
+++ setup-admin.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const pool = mysql.createPool({
